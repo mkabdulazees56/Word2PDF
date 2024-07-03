d6c34c44f882e8ceb7b8f819dc112f08bcfac768
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-07-04 00:07:28 +0500

    Loading icon added to Button

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import { FaFileWord } from "react-icons/fa6";
 import axios from "axios";
+import { FaSpinner } from "react-icons/fa"; 
+
 function Home() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [convert, setConvert] = useState("");
   const [downloadError, setDownloadError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleFileChange = (e) => {
-    // console.log(e.target.files[0]);
     setSelectedFile(e.target.files[0]);
   };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!selectedFile) {
@@ -18,6 +21,7 @@ function Home() {
     }
     const formData = new FormData();
     formData.append("file", selectedFile);
+    setLoading(true); 
     try {
       const response = await axios.post(
         "http://localhost:3000/convertFile",
@@ -26,34 +30,30 @@ function Home() {
           responseType: "blob",
         }
       );
-      console.log(response.data);
       const url = window.URL.createObjectURL(new Blob([response.data]));
-      console.log(url);
       const link = document.createElement("a");
-      console.log(link);
       link.href = url;
-      console.log(link);
       link.setAttribute(
         "download",
         selectedFile.name.replace(/\.[^/.]+$/, "") + ".pdf"
       );
-      console.log(link);
       document.body.appendChild(link);
-      console.log(link);
       link.click();
       link.parentNode.removeChild(link);
       setSelectedFile(null);
       setDownloadError("");
       setConvert("File Converted Successfully");
     } catch (error) {
-      console.log(error);
       if (error.response && error.response.status == 400) {
-        setDownloadError("Error occurred: ", error.response.data.message);
+        setDownloadError("Error occurred: " + error.response.data.message);
       } else {
         setConvert("");
       }
+    } finally {
+      setLoading(false); 
     }
   };
+
   return (
     <>
       <div className="max-w-screen-2xl mx-auto container px-6 py-3 md:px-40">
@@ -86,9 +86,12 @@ function Home() {
               </label>
               <button
                 onClick={handleSubmit}
-                disabled={!selectedFile}
-                className="text-white bg-blue-500 hover:bg-blue-700 disabled:bg-gray-400 disabled:pointer-events-none duration-300 font-bold px-4 py-2 rounded-lg"
+                disabled={!selectedFile || loading} 
+                className="text-white bg-blue-500 hover:bg-blue-700 disabled:bg-gray-400 disabled:pointer-events-none duration-300 font-bold px-4 py-2 rounded-lg flex items-center"
               >
+                {loading && (
+                  <FaSpinner className="animate-spin mr-2" /> 
+                )}
                 Convert File
               </button>
               {convert && (

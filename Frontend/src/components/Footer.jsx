9d5ce023c7aace4ece7eefeffc0544df4e09d796
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-07-03 23:22:51 +0500

    footer updated

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className="text-center text-gray-400">
+      © {currentYear} All rights reserved. Development with{" "}
+      <span className="text-pink-500">❤</span> by{" "}
+      <a
+        href="https://mkabdulazees.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-pink-500 hover:underline"
+      >
+        DevZee
+      </a>
+      .
+    </div>
+  );
+}
+
+export default Footer;
